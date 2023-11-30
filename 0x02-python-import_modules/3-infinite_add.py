c89ef535e9f421de51e0f504f0c0a320323c825f
BnBonei <aa7726956@gmail.com>
2023-11-30 07:38:12 -0800

    python2

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import sys, math
-    result = 0
-    for i in sys.argv:
-        result += int(i)
-        print("{}".format(result))
+from sys import argv
+
+
+def main():
+    sum = 0
+
+    for count, args in enumerate(argv):
+        if count == 0:
+            continue
+
+        sum += int(args)
+
+    print(f"{sum:d}")
+
+
+if __name__ == '__main__':
+    main()
