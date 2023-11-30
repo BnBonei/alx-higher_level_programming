d6f7326fb0e73ef8d9e84dbb169f5c13a98fccd1
BnBonei <aa7726956@gmail.com>
2023-11-30 07:42:44 -0800

    python2

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
-    n = len(sys.argv) - 1
-    if n == 0:
-        print("0 arguments.")
-    elif n == 1:
-        print("1 argument:")
-    else:
-        print("{} arguments:".format(n))
-    for i in range(n):
-        print("{}: {}".format(i + 1, sys.argv[i + 1]))
+    import hidden_4
+    for name in dir(hidden_4):
+        if name[0] != '_' and name[1] != '_':
+            print(name)
